6039c6dbee1b17d3bcf4e5d157cbd6e71bf960e9
Kevin Zhao <kevin8093@126.com>
2024-09-21 08:37:38 +0800

    update

--- dify-cdk/bin/dify-helm-stack.ts
+++ dify-cdk/bin/dify-helm-stack.ts
@@ -191,7 +191,7 @@ export class DifyHelmStack extends cdk.Stack {
         },
 
         frontend: {
-          replicaCount: 1,
+          replicaCount: 2,
 
           image: {
             repository: 'langgenius/dify-web',
@@ -253,7 +253,7 @@ export class DifyHelmStack extends cdk.Stack {
         },
 
         api: {
-          replicaCount: 1,
+          replicaCount: 2,
           image: {
             repository: 'langgenius/dify-api',
             pullPolicy: 'IfNotPresent',
@@ -308,7 +308,7 @@ export class DifyHelmStack extends cdk.Stack {
         },
 
         worker: {
-          replicaCount: 1,
+          replicaCount: 2,
           image: {
             repository: 'langgenius/dify-api',
             pullPolicy: 'IfNotPresent',
@@ -334,7 +334,7 @@ export class DifyHelmStack extends cdk.Stack {
         },
 
         sandbox: {
-          replicaCount: 1,
+          replicaCount: 2,
           apiKey: 'dify-sandbox', 
           apiKeySecret: '', 
           image: {
